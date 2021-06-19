1cf8fea0f9ded997212eef66ecee14f9d2d816b7
ibizaSunrise <milalemekh@gmail.com>
2021-06-19 20:50:53 +0200

    Add Navbar component and About pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function About() {
     return (
-        <div>
-            About
+        <div class="hero-unit" py-4>
+            <h1>Записная книжка</h1>
+            <p>Версия 1.0.42</p>
+            <p>
+
+            </p>
         </div>
     )
 }

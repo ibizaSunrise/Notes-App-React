9395ab83b556a9f2077c0b45d91840d9ed1240f5
ibizaSunrise <milalemekh@gmail.com>
2021-06-21 19:32:24 +0200

    Add context to Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { AlertContext } from '../context/alert/alertContext';
 
-export default function Alert({alert}) {
+
+export default function Alert() {
+    const {alert, hide} = useContext(AlertContext)
     if(!alert){
         return null;
     }
     return (
         <div className={`alert alert-${alert.type || 'warning'} alert-dismissible`}>
             <strong>Внимание!</strong>{alert.text}
-            <button type="button" className="btn-close" aria-label="Close">
+            <button onClick = {hide} type="button" className="btn-close" aria-label="Close">
                 <span aria-hidden = 'true'>&times;</span>
             </button>
         </div>

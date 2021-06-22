122adc11785da8e089ab9f33c4da08c9cf175c89
ibizaSunrise <milalemekh@gmail.com>
2021-06-22 17:37:43 +0200

    Add folder firebase, add firebase config, add firebaseContext, firebaseReducer, FirebaseState

---
+++ src/context/firebase/FirebaseState.js
@@ -0,0 +1,18 @@
+import React, {useReducer} from 'react';
+import { FirebaseContext } from './firebaseContext';
+import {firebaseReducer} from './firebaseReducer'
+
+export const FirebaseState = ({children}) =>{
+    const initialState ={
+        notes: [],
+        loading: false
+    }
+    const [state, dispatch] = useReducer(firebaseReducer, initialState)
+    return (
+        <FirebaseContext.Provider value={{
+            
+        }}>
+            {children}
+        </FirebaseContext.Provider>
+    )
+}
